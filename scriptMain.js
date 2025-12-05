df7bd4f666c956e262d82a78980975510f91c572
fayzonofficial <fayzonoff@gmail.com>
2025-12-05 20:23:59 +0600

    Update scriptMain.js

---
+++ scriptMain.js
@@ -0,0 +1,15 @@
+document.getElementById("cardSite").addEventListener("click", function () {
+    window.open("card.html", "_blank"); 
+});
+
+document.getElementById("cardSite2").addEventListener("click", function () {
+    window.open("card.html", "_blank");  
+});
+
+document.getElementById("cardSite3").addEventListener("click", function () {
+    window.open("card.html", "_blank"); 
+});
+
+document.getElementById("cardSite4").addEventListener("click", function () {
+    window.open("card.html", "_blank");  
+});
